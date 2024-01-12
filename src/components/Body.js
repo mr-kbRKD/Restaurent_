4b7e3220c11223e3904544d1be488abc306383fb
KULBHUSHAN <krishnathakur0007895@gmail.com>
2024-01-12 23:28:20 +0530

    Added basic search functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,22 +1,58 @@
 import { restaurentList } from "../../config"
 import RestaurentCard from "./ResturentCard"
+import { useState } from "react";
 
-const Body = () =>{
+
+
+function filterData(searchTxt, restaurants) {
+  const filteredData = restaurants.filter((restaurant) => restaurant.name.toLowerCase().includes(searchTxt.toLowerCase()))
+
+  return filteredData;
+}
+
+
+
+
+const Body = () => {
+  // let searchTxt = "Balram";
+
+  const [restaurants, setrestaurants] = useState(restaurentList);
+  const [searchTxt, setsearchTxt] = useState("");
+
   return (
-      <>
-      <div className="restaurent-list">
-         <RestaurentCard {...restaurentList[0].data} />
-         {/* <RestaurentCard restaurent = {restaurentList[1]}/> */}
-         {/* <RestaurentCard />
-         <RestaurentCard />
-         <RestaurentCard />
-         <RestaurentCard />
-         <RestaurentCard />
-         <RestaurentCard />
-         <RestaurentCard />
-         <RestaurentCard /> */}
+    <>
+
+      <div className="search-container">
+        <input type="text"
+          className="search-input"
+          placeholder="search"
+          value={searchTxt}
+          onChange={(e) => {
+            setsearchTxt(e.target.value);
+          }}
+        />
+        <button className="search-btn" onClick={() => {
+          // need to filter the data
+          const data = filterData(searchTxt, restaurants);
+          setrestaurants(data);
+        }}
+        > Search it</button>
       </div>
-      </>
+
+      <div className="restaurent-list">
+      {/* manual way */}
+        {/* <RestaurentCard {...restaurentList[0]} />
+        <RestaurentCard {...restaurentList[1]} /> */}
+        {/* <RestaurentCard restaurent = {restaurentList[1]}/> */}
+
+        {/* iterating */}
+        {restaurants.map((restaurant) => {
+          return (
+            <RestaurentCard {...restaurant} />
+          );
+        })}
+      </div>
+    </>
   )
 }
 
